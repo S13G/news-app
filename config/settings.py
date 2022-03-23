776c20a095a3db06eaef9d6bbb025f4e5df07aa7
s13g <ayflix0@gmail.com>
2022-03-23 14:48:30 +0100

    git update

--- config/settings.py
+++ config/settings.py
@@ -10,11 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
-from environs import Env
 from pathlib import Path
-
-env = Env()
-env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
